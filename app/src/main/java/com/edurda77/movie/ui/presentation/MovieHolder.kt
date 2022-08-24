479bdbf35521aa8fd9e1ef9c4482538af24be018
hayarovy <edurd@rambler.ru>
2022-08-24 16:26:41 +0500

    release load top movies without pagination

--- app/src/main/java/com/edurda77/movie/ui/presentation/MovieHolder.kt
+++ app/src/main/java/com/edurda77/movie/ui/presentation/MovieHolder.kt
@@ -7,7 +7,7 @@ import com.edurda77.movie.databinding.ItemGridViewBinding
 import com.edurda77.movie.entity.MovieInList
 import com.edurda77.movie.utils.BASE_URL_POSTER
 import com.edurda77.movie.utils.colorRating
-import kotlin.math.roundToInt
+import com.edurda77.movie.utils.popularityConvertor
 
 class MovieHolder(private val binding: ItemGridViewBinding):
     RecyclerView.ViewHolder(binding.root) {
@@ -15,9 +15,10 @@ class MovieHolder(private val binding: ItemGridViewBinding):
     fun bind(moveInList: MovieInList) {
         binding.dateRelease.text = moveInList.dateRelease
         binding.titleMovie.text = moveInList.title
-        binding.txtProgress.text = moveInList.rating.toString()
-        binding.rang.progress = moveInList.rating.roundToInt()
-        binding.rang.setIndicatorColor(colorRating(moveInList.rating.roundToInt()))
+        val ratingInt = popularityConvertor(moveInList.rating)
+        binding.txtProgress.text = ratingInt.toString()
+        binding.rang.progress = ratingInt
+        binding.rang.setIndicatorColor(colorRating(ratingInt))
         val urlPoster = BASE_URL_POSTER+moveInList.pathPoster
         Glide.with(this.itemView.context)
             .load(urlPoster)
